734da14eb27a441e89c9ff8709b7a3414e948260
TheoLeyenda <theoleyenda1997@gmail.com>
2026-01-13 21:34:43 -0300

    Update Gameplay

--- ARQVJ2026-main/Assets/Code/Architecture/Gameplay.cs
+++ ARQVJ2026-main/Assets/Code/Architecture/Gameplay.cs
@@ -1,20 +1,26 @@
 using TheoLeyenda.ToolBox.Updateable;
 using TheoLeyenda.ToolBox.EventBus;
 using TheoLeyenda.ToolBox.ServiceProvider;
+using TheoLeyenda.ToolBox.Scheduling;
 
 namespace ZooArchitect.Architecture
 {
     public sealed class Gameplay : IUpdateable
     {
         public EventBus EventBus => ServiceProvider.Instance.GetService<EventBus>();
+        public TaskScheduler TaskScheduler => ServiceProvider.Instance.GetService<TaskScheduler>();
 
         public Gameplay()
         {
             ServiceProvider.Instance.AddService<EventBus>(new EventBus());
+            ServiceProvider.Instance.AddService<TaskScheduler>(new TaskScheduler());
         }
 
         public void Init() {}
 
-        public void Update(float deltaTime){}
+        public void Update(float deltaTime)
+        {
+            TaskScheduler.Update(deltaTime);
+        }
     }
 }
